657833e31fe21aa803278046832f0862064d9846
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-03 05:30:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ function shout(words){
 function whisper(words){
   return words.toLowerCase();
 }
+
+function logShout(words){
+  console.log(shout(words));
+}
+
+function logWhisper(words){
+  console.log(whisper(words));
+}
+
+function sayHiToGrandma(words){
+  if(words === "I love you, Grandma"){
+    return "I love you, too.";
+  } else if(words === whisper(words)){
+    return "I can't hear you!";
+  } else if(words === shout(words)){
+    return "YES INDEED";
+  } else {
+    return "Grandma seems confused."
+  }
+}
